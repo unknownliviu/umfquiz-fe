bc14c39ec150254b129048bc4ece23cb05b26767
Andrei Constantinescu <andrei@constantinescu.io>
2017-10-17 10:05:28 +0100

    🐛 Fix navigation bug. Closes #10.

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import { Link } from 'react-router-dom'
-
+import React from 'react'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import {Link} from 'react-router-dom'
 
 export default class DrawerUndockedExample extends React.Component {
-
   constructor(props) {
-    super(props);
-    this.state = {open: false};
+    super(props)
+    this.state = {open: false}
   }
 
-  handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle = () => this.setState({open: !this.state.open})
 
-  handleClose = () => this.setState({open: false});
+  handleClose = () => this.setState({open: false})
 
   render() {
     return (
@@ -21,14 +19,29 @@ export default class DrawerUndockedExample extends React.Component {
         <Drawer
           docked={false}
           width={200}
-          open={ this.props.isOpened}
+          open={this.props.isOpened}
           onRequestChange={this.props.onRequestChange}
         >
-          <MenuItem><Link to="/" onClick={this.props.handleClose}>Home</Link></MenuItem>
-          <MenuItem><Link to="/about" onClick={this.props.handleClose}>About</Link></MenuItem>
-          <MenuItem><Link to="/salam" onClick={this.props.handleClose}>Florin Salam</Link></MenuItem>
+          <MenuItem
+            linkButton
+            primaryText="Home"
+            containerElement={<Link to="/" />}
+            onClick={this.props.handleClose}
+          />
+          <MenuItem
+            linkButton
+            primaryText="About"
+            containerElement={<Link to="/about" />}
+            onClick={this.props.handleClose}
+          />
+          <MenuItem
+            linkButton
+            primaryText="Florin Salam"
+            containerElement={<Link to="/slam" />}
+            onClick={this.props.handleClose}
+          />
         </Drawer>
       </div>
-    );
+    )
   }
 }
