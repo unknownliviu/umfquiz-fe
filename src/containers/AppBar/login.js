56ed79f37c17a413bfba4dfbaf8310c6f9eb295d
Andrei Constantinescu <andrei@constantinescu.io>
2017-10-17 09:11:17 +0100

    ✨ Add facebook login.

--- src/containers/AppBar/login.js
+++ src/containers/AppBar/login.js
@@ -4,12 +4,37 @@ import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import IconButton from 'material-ui/IconButton'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+import FacebookLogin from 'react-facebook-login'
+import ActionAndroid from 'material-ui/svg-icons/action/android'
+import {ToolbarTitle, ToolbarGroup} from 'material-ui/Toolbar'
+const responseFacebook = response => {
+  console.log(response)
+}
 
 class Login extends PureComponent {
   static muiName = 'FlatButton'
 
   render() {
-    return <FlatButton label="Login" {...this.props} />
+    const {handleLogin} = this.props
+    return (
+      <ToolbarGroup>
+        <FlatButton
+          {...this.props}
+          children={
+            <FacebookLogin
+              appId={process.env.REACT_APP_FB_APP_ID}
+              autoLoad={true}
+              fields="name,email,picture"
+              callback={handleLogin}
+              cssClass="fb-login-button"
+              tag="div"
+              textButton="Login"
+              size="medium"
+            />
+          }
+        />
+      </ToolbarGroup>
+    )
   }
 }
 
@@ -17,21 +42,21 @@ class LoggedIn extends PureComponent {
   static muiName = 'IconMenu'
   render() {
     return (
-      <IconMenu
-        {...this.props}
-        iconButtonElement={
-          <div>
-            {this.props.name}
+      <ToolbarGroup>
+        <ToolbarTitle text={this.props.name} />
+        <IconMenu
+          {...this.props}
+          iconButtonElement={
             <IconButton>
               <MoreVertIcon />
             </IconButton>
-          </div>
-        }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-      >
-        <MenuItem primaryText="Sign out" onClick={this.props.handleLogout} />
-      </IconMenu>
+          }
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        >
+          <MenuItem primaryText="Sign out" onClick={this.props.handleLogout} />
+        </IconMenu>
+      </ToolbarGroup>
     )
   }
 }
